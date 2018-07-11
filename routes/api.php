5d7480ad686167d9ef4da7fa4e6c919e654bc285
Vladislav <vladislav.khlystun@gmail.com>
2018-07-11 15:09:35 +0300

    initial commit

---
+++ routes/api.php
@@ -0,0 +1,8 @@
+
+<?php
+Route::post('register', 'AuthController@register');
+Route::post('login', 'AuthController@login');
+Route::post('recover', 'AuthController@recover');
+Route::group(['middleware' => ['jwt.auth']], function() {
+    Route::get('logout', 'AuthController@logout');
+});
